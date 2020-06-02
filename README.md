363289cba15d120c2673ba6864be7a6661f5c8b8
Statescu Razvan <razvan_statescu@yahoo.com>
2020-06-02 14:40:25 +0300

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mask Detection using Machine Learning 
+
+This repository is associated with [this YouTube video](https://youtu.be/bCFtoUm5uH4).
+
+In [this](https://youtu.be/bCFtoUm5uH4) video we made an app that detects if people are wearing surgical masks or not. 
+We ueed [tensorflow](https://www.tensorflow.org/js) with the [mobilenet model](https://github.com/tensorflow/tfjs-models/tree/master/mobilenet) and [knn classifier](https://github.com/tensorflow/tfjs-models/tree/master/knn-classifier) to classify the images.
+
+## Install and use the application
+
+In order to install and use the application you just have to clone the repository and open the `index.html` file.
---
+++ app.js
@@ -0,0 +1,60 @@
+window.onload = async () => {
+    const maskImageCount = 5;
+    const noMaskImageCount = 6;
+
+    const trainImagesContainer = document.querySelector('.train-images');
+    // Add mask images to the DOM and give them a class of `mask-img`
+    for (let i = 1; i <= maskImageCount; i++) {
+        const newImage = document.createElement('IMG');
+        newImage.setAttribute('src', `images/mask/${i}.jpg`);
+        newImage.classList.add('mask-img');
+        trainImagesContainer.appendChild(newImage);
+    }
+    // Add no mask images to the DOM and give them a class of `no-mask-img`
+    for (let i = 1; i <= noMaskImageCount; i++) {
+        const newImage = document.createElement('IMG');
+        newImage.setAttribute('src', `images/no_mask/${i}.jpg`);
+        newImage.classList.add('no-mask-img');
+        trainImagesContainer.appendChild(newImage);
+    }
+
+    // Load mobilenet module
+    const mobilenetModule = await mobilenet.load({version: 2, alpha: 1});
+    // Add examples to the KNN Classifier
+    const classifier = await trainClassifier(mobilenetModule);
+
+    // Predict class for the test image
+    const testImage = document.getElementById('test-img');
+    const tfTestImage = tf.browser.fromPixels(testImage);
+    const logits = mobilenetModule.infer(tfTestImage, 'conv_preds');
+    const prediction = await classifier.predictClass(logits);
+
+    // Add a border to the test image to display the prediction result
+    if (prediction.label == 1) { // no mask - red border
+        testImage.classList.add('no-mask');
+    } else { // has mask - green border
+        testImage.classList.add('mask');
+    }
+};
+
+async function trainClassifier(mobilenetModule) {
+    // Create a new KNN Classifier
+    const classifier = knnClassifier.create();
+
+    // Train using mask images
+    const maskImages = document.querySelectorAll('.mask-img');
+    maskImages.forEach(img => {
+        const tfImg = tf.browser.fromPixels(img);
+        const logits = mobilenetModule.infer(tfImg, 'conv_preds');
+        classifier.addExample(logits, 0); // has mask
+    });
+    // Train using no mask images
+    const noMaskImages = document.querySelectorAll('.no-mask-img');
+    noMaskImages.forEach(img => {
+        const tfImg = tf.browser.fromPixels(img);
+        const logits = mobilenetModule.infer(tfImg, 'conv_preds');
+        classifier.addExample(logits, 1); // no mask
+    });
+
+    return classifier;
+}
